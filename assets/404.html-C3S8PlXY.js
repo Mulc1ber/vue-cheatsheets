a60a2b3ce035019291de6d17b6d1ddabdcfc5906
Mulc1ber <dmo@mail.ru>
2024-07-20 22:11:34 +0300

    add link git

---
+++ assets/404.html-C3S8PlXY.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o,a}from"./app-aE-8dwiI.js";const n={},c=a("p",null,"404 Not Found",-1),l=[c];function s(r,_){return o(),e("div",null,l)}const d=t(n,[["render",s],["__file","404.html.vue"]]),u=JSON.parse('{"path":"/404.html","title":"","lang":"ru-RU","frontmatter":{"layout":"NotFound"},"headers":[],"git":{},"filePathRelative":null}');export{d as comp,u as data};
